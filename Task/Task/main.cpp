e00ea65ed4d178204d37e224b659db1e2afbd992
古泉達也 <koizumi@ridi.co.jp>
2019-02-24 09:24:57 +0900

    じゃんけん修正

--- Task/Task/main.cpp
+++ Task/Task/main.cpp
@@ -1,17 +1,44 @@
+#include <time.h>
 #include "main.h"
 
+#define JUDGE_TYPE_MAX 3
+// 実体定義
+JudgeInfo m_JudgeInfo;
 
+std::map<JudgeType, JudgeInfo> m_judge =
+{
+	{ Win,	JudgeInfo{ "勝ち" ,0 } },
+	{ Lose,	JudgeInfo{ "負け" ,0 } },
+	{ Draw,	JudgeInfo{ "引き分け" ,0 } },
+};
+
+enum AttackTYPE
+{
+	Rock = 1,	// グー
+	Scissors,	// チョキ
+	Paper,		// パー
+	TYPE_MAX,
+};
+
+// 名前
+const std::map<AttackTYPE, std::string> m_name =
+{
+	{ Rock,		"グー" },
+	{ Scissors,	"チョキ" },
+	{ Paper,	"パー" },
+};
+// 勝敗[プレイヤー][CPU]
+JudgeType m_judgeType[JUDGE_TYPE_MAX][JUDGE_TYPE_MAX] =
+{
+	{ Draw,Win,Lose },	// グー 
+	{ Lose,Draw,Win },	// チョキ
+	{ Win,Lose,Draw },	// パー 
+};
 
 //勝ち負け判定
-JudgeType WinLoseJudge(AttackTYPE p, AttackTYPE e)
+JudgeType WinLoseJudge(AttackTYPE player, AttackTYPE enemy)
 {
-	// デフォルトは引き分け
-	JudgeType type = Draw;
-	// 勝ち負け判定
-	type = m_typeMap[p] == e ? Win : type;
-	type = m_typeMap[e] == p ? Lose : type;
-
-	return type;
+	return m_judgeType[player - 1][enemy - 1];
 }
 
 int main()
@@ -43,18 +70,18 @@ int main()
 		enemy = rand() % (TYPE_MAX - 1) + 1;
 
 		// 選択したやつ
-		printf("プレイヤーは%sを選択しました\n", m_name[(AttackTYPE)player].c_str());
-		printf("相手は%sを選択しました\n", m_name[(AttackTYPE)enemy].c_str());
+		printf("プレイヤーは%sを選択しました\n", m_name.at((AttackTYPE)player).c_str());
+		printf("相手は%sを選択しました\n", m_name.at((AttackTYPE)enemy).c_str());
 		
 		JudgeType judge = WinLoseJudge((AttackTYPE)player, (AttackTYPE)enemy);
 		// カウント増やす
-		m_judgeInfo[(JudgeType)judge].count += 1;
+		m_judge[(JudgeType)judge].count += 1;
 		
 		// 勝ち負け判定
-		printf("%s\n", m_judgeInfo[(JudgeType)judge].name.c_str());
+		printf("%s\n", m_judge[(JudgeType)judge].name.c_str());
 
 		// 戦績
-		printf("勝ち%d 負け%d 引き分け%d\n\n", m_judgeInfo[Win].count, m_judgeInfo[Lose].count, m_judgeInfo[Draw].count);
+		printf("勝ち%d 負け%d 引き分け%d\n\n", m_judge[Win].count, m_judge[Lose].count, m_judge[Draw].count);
 	}
 }
 
